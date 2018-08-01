803e6b213eef7a52b665e2ddfa31c6c462a0d539
John Dawes <jdawes@bluebitmedia.com>
2018-08-01 16:55:58 -0600

    CSS Animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  // Sticky Navigation
   $("#features").waypoint(
     function(direction) {
       if (direction == "down") {
@@ -9,6 +10,72 @@ $(document).ready(function() {
     },
     {
       offset: "60px"
+    }
+  );
+
+  // Smooth scrolling
+  // NOTE: need full version (NOT slim) of jquery for this to work
+  $("nav a, .hero-text-box a").on("click", function(e) {
+    // Check for hash value on clicked link
+    if (this.hash !== "") {
+      // Prevent default behavior
+      e.preventDefault();
+
+      // store hash # (anchor string)
+      const hash = this.hash;
+
+      // Animate smooth scroll
+      $("html, body").animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        1000,
+        function() {
+          // Add hash/anchor string to URL after scroll
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
+
+  // Animations
+  // #features
+  $(".js--animate-1").waypoint(
+    function(direction) {
+      $(".js--animate-1").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  // #how-it-works
+  $(".js--animate-2").waypoint(
+    function(direction) {
+      $(".js--animate-2").addClass("animated fadeInUp");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  // #locations
+  $(".js--animate-3").waypoint(
+    function(direction) {
+      $(".js--animate-3").addClass("animated fadeIn");
+    },
+    {
+      offset: "50%"
+    }
+  );
+
+  // #pricing
+  $(".js--animate-4").waypoint(
+    function(direction) {
+      $(".js--animate-4").addClass("animated pulse");
+    },
+    {
+      offset: "50%"
     }
   );
 });
