e3f62fc6eabd93aef15084321e293b82acc6ab38
John Dawes <jdawes@bluebitmedia.com>
2018-08-01 15:57:55 -0600

    Responsive tweaks

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(function() {
-  // Init
+  $("#features").waypoint(
+    function(direction) {
+      if (direction == "down") {
+        $("nav").addClass("sticky-nav");
+      } else {
+        $("nav").removeClass("sticky-nav");
+      }
+    },
+    {
+      offset: "60px"
+    }
+  );
 });
