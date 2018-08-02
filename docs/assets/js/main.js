c01eaf671fb509b3e4a4a4db356d4c821b401995
John Dawes <jdawes@bluebitmedia.com>
2018-08-02 12:01:07 -0600

    Image compression

--- docs/assets/js/main.js
+++ docs/assets/js/main.js
@@ -1,4 +1,20 @@
 $(document).ready(function() {
+  // Navigation Toggler Icon & Show/Hide Mobile sticky navbar
+  $(".nav-toggler").click(function(e) {
+    e.preventDefault();
+    var nav = $(".main-nav");
+    var icon = $(".nav-toggler i");
+
+    if (icon.hasClass("ion-navicon-round")) {
+      icon.addClass("ion-close-round");
+      icon.removeClass("ion-navicon-round");
+    } else {
+      icon.removeClass("ion-close-round");
+      icon.addClass("ion-navicon-round");
+    }
+    nav.slideToggle(200);
+  });
+
   // Sticky Navigation
   $("#features").waypoint(
     function(direction) {
